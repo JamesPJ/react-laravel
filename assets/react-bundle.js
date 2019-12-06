764fcd5a0d22570b7774cbfb989e0f13f96d2540
James PJ <jms.jpj@gmail.com>
2019-12-06 16:01:38 +0530

    Added react bundle

---
+++ assets/react-bundle.js
@@ -0,0 +1,4 @@
+// taken from https://github.com/reactjs/react-php-v8js/blob/master/example/src/react-bundle.js
+global.React = require('react');
+global.ReactDOM = require('react-dom');
+global.ReactDOMServer = require('react-dom/server');
